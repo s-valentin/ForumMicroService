1f917f6a748511e5229f3cadf7d5eb9d8810ac40
Vali <valentin.stanciu@info.uaic.ro>
2021-04-18 16:33:05 +0300

    adaugare spring in repo

---
+++ Forum/src/test/java/ro/uaic/info/Forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uaic.info.Forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
